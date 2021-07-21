2fe9bcee369056c42e218511cb75d55f0c51b437
Liam R <liamroberts77@googlemail.com>
2021-07-21 11:41:20 +0100

    added folder for styledbuttons and added boiler plate component

---
+++ components/StyledButton/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, ImageBackground } from "react-native";
+import styles from './styles';
+
+const StyledButton = () => {
+    return (
+        <View style={styles.}>
+
+      </View>
+    );
+};
+
+export default StyledButton;
