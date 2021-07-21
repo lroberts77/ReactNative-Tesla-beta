1073f50fc83b25b1e0ea3dc32c15a9afc92bd986
Liam R <liamroberts77@googlemail.com>
2021-07-21 13:59:07 +0100

    added styling for container in styledbutton/styles

--- components/StyledButton/styles.js
+++ components/StyledButton/styles.js
@@ -1,7 +1,16 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        padding: 10,
+    },
+    btn: {
 
+    },
+    text: {
+
+    },
 });
 
 export default styles;
