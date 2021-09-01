b01cdb8f7ce84e7b4c59ec725d177655aaf4a142
Liam R <liamroberts77@googlemail.com>
2021-09-01 17:53:50 +0100

    added styling for header container

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -2,7 +2,9 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container: {
-
+        position: 'absolute',
+        top: 50,
+        zIndex: 5,
     },
     logo: {
 
